80e4ef4674c3cae73dc88610d484e9b10e508cdf
awangelo <anggg.gaio@gmail.com>
2024-10-23 19:00:18 -0300

    feat: pages structure

---
+++ src/app/pages/comprar/comprar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comprar',
+  standalone: true,
+  imports: [],
+  templateUrl: './comprar.component.html',
+  styleUrl: './comprar.component.scss'
+})
+export class ComprarComponent {
+
+}
---
+++ src/app/pages/criadores/criadores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criadores',
+  standalone: true,
+  imports: [],
+  templateUrl: './criadores.component.html',
+  styleUrl: './criadores.component.scss'
+})
+export class CriadoresComponent {
+
+}
