98462e2e243585b36b3786a0817cb762d38f48c0
awangelo <anggg.gaio@gmail.com>
2024-10-23 19:00:43 -0300

    feat: base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ComprarComponent } from './pages/comprar/comprar.component';
+import { CriadoresComponent } from './pages/criadores/criadores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },           
+    { path: 'comprar', component: ComprarComponent }, 
+    { path: 'criadores', component: CriadoresComponent }
+];
