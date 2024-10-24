91bd72f65ab3d0a5acca588d8439229a7bbbdb03
awangelo <anggg.gaio@gmail.com>
2024-10-24 19:33:17 -0300

    feat: footer structure & test colors

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  leftUrl: string = 'https://github.com/awangelo/Monitoria-Angular';
+  middleUrl: string = 'https://www.instagram.com/monitoriatech/';
 }
